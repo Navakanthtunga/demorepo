21c646d8c662b41ef8f1e139206557e904f123c8
Navakanth Tunga <THI1804367@thi.org>
2019-10-13 00:40:38 +0530

    Added DemoClass2 in test folder

--- src/test/java/com/gitdemo1/pack1/DemoClass2.java
+++ src/test/java/com/gitdemo1/pack1/DemoClass2.java
@@ -1,9 +1,7 @@
 package com.gitdemo1.pack1;
 
-public class DemoClass {
+public class DemoClass2 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		
-		System.out.println("Hello World 2");
 	}
 }
